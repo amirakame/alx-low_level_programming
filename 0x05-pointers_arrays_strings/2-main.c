2ed044e4c2f1db69d21ead7c80b1c7dbf646059c
amirakame <amirakamel080@gmail.com>
2023-03-27 18:31:18 -0700

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -0,0 +1,19 @@
+julien@ubuntu:~/0x05$ cat 2-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+	int len;
+
+	str = "My first strlen!";
+	len = _strlen(str);
+	printf("%d\n", len);
+	return (0);
+}
