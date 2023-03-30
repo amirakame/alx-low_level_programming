fcf2e90fafcf9aaf565ae4ce2e57599ca6dea6a7
amirakame <amirakamel080@gmail.com>
2023-03-30 14:42:12 -0700

    ponters arrays strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,12 @@
 #include "main.h"
 /**
- * _strncat - concatenate two strings
- * using at most n bytes from src
+ * _strcat - concatenates two strings
  * @dest: input value
  * @src: input value
- * @n: input value
  *
- * Return: dest
+ * Return: void
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
@@ -19,12 +17,13 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	}
 	j = 0;
-	while (j < n && src[j] != '\0')
+	while (src[j] != '\0')
 	{
-	dest[i] = src[j];
-	i++;
-	j++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
+
 	dest[i] = '\0';
 	return (dest);
 }
