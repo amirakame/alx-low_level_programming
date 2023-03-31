fc62194c3cc1cbce93773ed0ac05e99b946db103
amirakame <amirakamel080@gmail.com>
2023-03-31 07:12:24 -0700

    poniters arrays strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -8,12 +8,12 @@
 */
 void reverse_array(int *a, int n)
 {
-		int i;
-			int t;
+	int i;
+	int t;
 
-for (i = 0 ; i < n-- ; i++)
-					{
-									t = a[i];
+	for (i = 0 ; i < n-- ; i++)
+	{
+	t = a[i];
 a[i] = a[n];
 a[n] = t;
 }
