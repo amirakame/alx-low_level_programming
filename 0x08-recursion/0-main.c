139081109ef3967565c124806c103f776d85dfc7
amirakame <amirakamel080@gmail.com>
2023-04-05 06:54:12 -0700

    amira

--- 0x08-recursion/0-main.c
+++ 0x08-recursion/0-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
+   * main - check the code
+    *
+     * Return: Always 0.
+      */
 int main(void)
 {
-	_puts_recursion("Puts with recursion");
-	return (0);
+	    _puts_recursion("Puts with recursion");
+return (0);
 }
