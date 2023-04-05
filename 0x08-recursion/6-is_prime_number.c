c8dbdfbf8941f73f95125fdd20954cd28074f04e
amirakame <amirakamel080@gmail.com>
2023-04-05 12:11:32 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int actual_prime(int n, int i);
+
 /**
    * is_prime_number - checks if a number is prime
     * @n: the number to check
@@ -18,6 +20,5 @@ for (i = 2; i * i <= n; i++)
 if (n % i == 0)
 return (0);
 }
-
 return (1);
 }
