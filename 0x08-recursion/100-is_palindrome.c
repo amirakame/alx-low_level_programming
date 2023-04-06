0d1425b812fbf8c4a302be7451dd942ca60fcc95
amirakame <amirakamel080@gmail.com>
2023-04-06 13:52:18 -0700

    task 100

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,42 +1,45 @@
 #include "main.h"
-#include <stdio.h>
+
+int is_palindrome_recursion(char *s, int start, int end);
 
 /**
    * is_palindrome - checks if a string is a palindrome
-    * @s: the string to check
+    * @s: string to check
      *
-      * Return: 1 if s is a palindrome, 0 otherwise
+      * Return: 1 if it is, 0 it's not
        */
 int is_palindrome(char *s)
 {
-	    int len = strlen(s);
+	    int len = 0;
+int i;
 
-/* base case: empty string or single-character string */
-if (len <= 1)
-return (1);
-		/* recursive case: check first and last characters */
-if (s[0] == s[len - 1])
-return (is_palindrome(s + 1) && is_palindrome(s, len - 2));
-else
+		    /* get length of string */
+while (s[len] != '\0')
+len++;
+
+/* compare first half of string to second half */
+for (i = 0; i < len / 2; i++)
+{
+if (s[i] != s[len - i - 1])
 return (0);
 }
 
-/**
-   * main - tests the is_palindrome function
-    *
-     * Return: always 0
-      */
-int main(void)
-{
-	    int r;
+return (1);
+}
 
-r = is_palindrome("level");
-		    printf("%d\n", r);
-r = is_palindrome("redder");
-			    printf("%d\n", r);
-r = is_palindrome("test");
-				    printf("%d\n", r);
-r = is_palindrome("step on no pets");
-					    printf("%d\n", r);
+/**
+   * is_palindrome_recursion - checks if a string is a palindrome, recursively
+    * @s: string to check
+     * @start: starting index of string
+      * @end: ending index of string
+       *
+* Return: 1 if it is, 0 it's not
+	 */
+int is_palindrome_recursion(char *s, int start, int end)
+{
+if (start >= end)
+return (1);
+if (s[start] != s[end])
 return (0);
+return (is_palindrome_recursion(s, start + 1, end - 1));
 }
