5138053bb09f1a30b9470ecb120ab345fcda3f24
amirakame <amirakamel080@gmail.com>
2023-04-09 17:51:53 -0700

    amira

--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -1,26 +1,33 @@
 #include "main.h"
+
 /**
    * _strcat - concatenates two strings
     * @dest: input value
      * @src: input value
       *
-       * Return: void
-*/
+       * Return: concatenated string
+        */
 char *_strcat(char *dest, char *src)
 {
-		int i;
-			int j;
-
-				i = 0;
-while (dest[i] != '\0')
-{
-i++;
+	    int i;
+	        int j;
+		    
+		    i = 0;
+		        while (dest[i] != '\0')
+				    {
+					            i++;
+						        }
+			    
+			    j = 0;
+			        while (src[j] != '\0')
+					    {
+						            dest[i] = src[j];
+							            i++;
+								            j++;
+									        }
+				    
+				    dest[i] = '\0'; /* add null character to end of concatenated string */
+				        
+				        return (dest);
 }
-j = 0;
-while (src[j] != '\0')
-{
-dest[i] = src[j];
-i++;
-j++;
 
-}
