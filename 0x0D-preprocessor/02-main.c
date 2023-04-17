f8b636444bfcae02aa8a1ae7459790364029e89b
amirakame <amirakamel080@gmail.com>
2023-04-17 15:29:04 -0700

    amira

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -2,12 +2,12 @@
 #define FILENAME "main.c"
 
 /**
-       * main - prints the name of the file
-*
-* Return: Always 0 (Success)
-*/
+   * main - prints the name of the file
+    *
+     * Return: Always 0 (Success)
+      */
 int main(void)
 {
-printf("The name of the current file is: %s\n", FILENAME);
+		printf("The name of the current file is: %s\n", FILENAME);
 return (0);
 }
