cae2e4fabdc1578bf2eb6b4bb58012c7f4906944
amirakame <amirakamel080@gmail.com>
2023-04-17 13:47:56 -0700

    amira

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+* free_dog - freeing the memory that was previously set aside for a struct dog
+    * @d: struct dog to free
+     */
+void free_dog(dog_t *d)
+{
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
