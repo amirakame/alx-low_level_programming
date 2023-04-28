901f3750e2430d811636836d700184bfa8206745
amirakame <amirakamel343@gmail.com>
2023-04-28 13:21:31 -0700

    chool

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -6,11 +6,11 @@ msg db "Hello, Holberton", 10
 
 section .text
 main:
-mov     rax, 1           ; system call for write
-mov     rdi, 1           ; file handle 1 is stdout
-mov     rsi, msg         ; address of string to output
-mov     rdx, 13          ; number of bytes
-syscall                   ; invoke operating system to do the write
-mov     rax, 60          ; system call for exit
-xor     rdi, rdi        ; exit code 0
-syscall                   ; invoke operating system to exit
+mov     rax, 1          ; system call for write
+mov     rdi, 1          ; file handle 1 is stdout
+mov     rsi, msg        ; address of string to output
+mov     rdx, 14         ; number of bytes
+syscall                  ; invoke operating system to do the write
+mov     rax, 60         ; system call for exit
+xor     rdi, rdi       ; exit code 0
+syscall                  ; invoke operating system to exit
