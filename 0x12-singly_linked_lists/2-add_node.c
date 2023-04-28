dffb43200d117f34235205d5b81e7604b7a94fa7
amirakame <amirakamel343@gmail.com>
2023-04-28 08:59:19 -0700

    chool

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,9 +3,9 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a linked list.
- * @head: double pointer to the list_t list.
- * @str: new string to add in the node.
+ * add_node - adds node to beginning's linked list.
+ * @head: two pointer's to list_t list.
+ * @str: string to add in the node.
  *
  * Return: the address of the new element, or NULL if it fails.
  */
