b7baa9663560618d0f8a22a1dca6fc3eb323d044
amirakame <amirakamel343@gmail.com>
2023-04-28 09:10:04 -0700

    chool

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,9 +3,9 @@
 #include "lists.h"
 
 /**
- * add_node_end - adds a new node at the end of a linked list.
- * @head: double pointer to the list_t list.
- * @str: string to put in the new node.
+ * add_node_end - adds's new node to end list.
+ * @head: two pointer's to list_t list.
+ * @str: string in put to node.
  *
  * Return: address of the new element, or NULL if it failed.
  */
