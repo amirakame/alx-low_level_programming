fdfdec40f2936323ce9d05c9049a794fee332857
amirakame <amirakamel343@gmail.com>
2023-04-28 08:44:47 -0700

    amira

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+global main
+extern printf
+
+section .data
+format db 'Hello, Holberton%s', 10, 0 ; format string for printf, with newline character and null byte
+
+section .text
+main:
+xor eax, eax ; clear rax
+push format ; push format string onto the stack
+call printf ; call printf with the format string on the stack
+
+add rsp, 8 ; remove arguments from the stack
+ret ; return from function
