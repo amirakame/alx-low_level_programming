3445903011cbbac368cc8da62751b96bc92d5171
amirakame <amirakamel343@gmail.com>
2023-04-29 14:30:26 -0700

    chool

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,28 @@
+/**
+ * print_listint_safe - Prints listint_t safely.
+ * @head: Pointer to the head node.
+ *
+ * Return: The number of nodes in the list.
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	const listint_t *p, *q;
+
+	p = head;
+
+	while (p != NULL)
+	{
+		count++;
+		printf("[%p] %d\n", (void *) p, p->n);
+		q = p;
+		p = p->next;
+		if (q < p)
+		{
+			fprintf(stderr, "Error: loop detected\n");
+			exit(98);
+		}
+	}
+
+	return (count);
+}
