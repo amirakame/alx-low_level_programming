1d8cf2179b7dda3cd704e60931c6b859887e6646
amirakame <amirakamel343@gmail.com>
2023-05-04 01:32:55 -0700

    chool

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+
+/**
+ * binary_to_uint - binary to uint.
+ * @b: pointer to a string of 0 and 1 chars.
+ *
+ * Return: the converted number, or 0 if there is one or more chars in the
+ * string b that is not 0 or 1. or if b is NULL.
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (*b != '\0')
+	{
+		if (*b != '0' && *b != '1')
+			return (0);
+
+		num <<= 1;
+		if (*b++ == '1')
+			num ^= 1;
+	}
+
+	return (num);
+}
