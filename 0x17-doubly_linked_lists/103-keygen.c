c55f22c214dcd9621dc57042466cf62650dd9bde
amirakame <amirakamel343@gmail.com>
2023-06-15 18:59:51 -0700

    chool

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,3 +1,8 @@
+ /*
+ * File: 103-keygen.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,28 +14,45 @@
  *
  * Return: Always 0.
  */
-
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	char *username = argv[1];
-	char *key;
+	char password[7], *codex;
+	int len = strlen(argv[1]), i, tmp;
 
-	if (argc != 2)
+	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+
+	tmp = (len ^ 59) & 63;
+	password[0] = codex[tmp];
+
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += argv[1][i];
+	password[1] = codex[(tmp ^ 79) & 63];
+
+	tmp = 1;
+	for (i = 0; i < len; i++)
+		tmp *= argv[1][i];
+	password[2] = codex[(tmp ^ 85) & 63];
+
+	tmp = 0;
+	for (i = 0; i < len; i++)
 	{
-		printf("Usage: %s username\n", argv[0]);
-		return (1);
+		if (argv[1][i] > tmp)
+			tmp = argv[1][i];
 	}
+	srand(tmp ^ 14);
+	password[3] = codex[rand() & 63];
 
-	if (strcmp(username, "julien") == 0)
-		key = "javascript";
-	else if (strcmp(username, "guillaume") == 0)
-		key = "lowlevelprog";
-	else
-	{
-		printf("No key found for %s\n", username);
-		return (1);
-	}
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += (argv[1][i] * argv[1][i]);
+	password[4] = codex[(tmp ^ 239) & 63];
 
-	printf("%s\n", key);
+	for (i = 0; i < argv[1][0]; i++)
+		tmp = rand();
+	password[5] = codex[(tmp ^ 229) & 63];
+
+	password[6] = '\0';
+	printf("%s", password);
 	return (0);
 }
